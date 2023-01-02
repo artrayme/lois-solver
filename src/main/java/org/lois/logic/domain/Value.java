b8ff3d2d7f13e14fbc53ba3f7343a801d65b0a53
straightth <speciallyfororigin@gmail.com>
2023-01-02 19:56:28 +0300

    Adding placeholder

--- src/main/java/org/lois/logic/domain/Value.java
+++ src/main/java/org/lois/logic/domain/Value.java
@@ -1,10 +1,30 @@
 package org.lois.logic.domain;
 
+import java.util.Arrays;
+
 @lombok.Value
 public class Value {
     boolean[] array;
 
-    static Value of(boolean[] array) {
+    public boolean[] getArray() {
+        if (array == null) {
+            throw new NullPointerException("Value can't be used without setting array");
+        }
+        return array;
+    }
+
+    public boolean[] getArrayCopy() {
+        return Arrays.copyOf(array, array.length);
+    }
+
+    public static Value of(boolean[] array) {
         return new Value(array);
+    }
+
+    /**
+     * @return Value without array inside it
+     */
+    public static Value placeholder() {
+        return new Value(null);
     }
 }
